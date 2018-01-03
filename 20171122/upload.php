4c0f4b243d190c2f8f3c4248cd606a1c6caedab6
zero951874 <32125252+zero951874@users.noreply.github.com>
2018-01-03 21:51:59 +0800

    Update upload.php

--- 20171122/upload.php
+++ 20171122/upload.php
@@ -3,6 +3,7 @@ $w = $_POST['a'];
 $h = $_POST['b'];
 if($w==null||$h==null){
     echo "輸入內容不能為空白<BR/>";
+    echo "<a href='javascript:window.history.back();'>回上一頁</a>";
 }
 else if(is_numeric($w)&&is_numeric($h)){
     $bmi=$w/($h*$h);
@@ -25,6 +26,7 @@ $ext = pathinfo($filename, PATHINFO_EXTENSION);
 if($filename ==null)
 {
     echo "請上傳圖片<BR/>";
+    echo "<a href='javascript:window.history.back();'>回上一頁</a>";
 }else if(!in_array($ext,$allowed) ) {
     echo "上傳失敗<BR/>";
     echo "上傳檔案為非圖片檔案類型";
